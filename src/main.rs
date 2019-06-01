0459731130c3b7b2cc18e476a6894702789a05fe
Nat Knight <nathaniel.ep@gmail.com>
2019-06-01 13:03:46 -0700

    Add BearLib dependency and extension libs

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,13 @@ mod model;
 mod newgame;
 
 fn main() {
-    println!("Hello, world!");
+    use bear_lib_terminal::terminal;
+
+    terminal::open("Test", 80, 30);
+    terminal::print_xy(0, 0, "Hello from rust");
+    terminal::refresh();
+
+    let _ = terminal::wait_event();
+
+    terminal::close();
 }
